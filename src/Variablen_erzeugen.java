4a69662fca7fd4feda330f0499e38bb2327b14ef
dkoenig_FIB26 <dkoenig@computer-manufaktur.de>
2023-01-20 17:08:00 +0100

    alles neu

---
+++ src/Variablen_erzeugen.java
@@ -0,0 +1,13 @@
+public class Variablen_erzeugen {
+
+    public static void main(String[] args) {
+
+        int age = 35; //immer mit Semmikolon abschließen <-- Initialisierung mit direktem Wert
+                    // bzw.
+        int age2;  // es gibt 2 Schreibweisen <-- Deklaration (Variable erzeugen "Datentyp" "Bezeichner")
+        age2 = 36; // "Zuweisung" (compiler Bezeichner wird mit dem Datentyp bekannt gemacht)
+
+        String name = David;
+
+    }
+}
