20f720f76b37ec00c7f2b721fe63127595107e5f
dkoenig_FIB26 <dkoenig@computer-manufaktur.de>
2023-01-24 10:31:09 +0100

    alles neu

--- src/Variablen_erzeugen.java
+++ src/Variablen_erzeugen.java
@@ -1,3 +1,5 @@
+import java.net.URLConnection;
+
 public class Variablen_erzeugen {
 
     public static void main(String[] args) {
@@ -5,9 +7,18 @@ public class Variablen_erzeugen {
         int age = 35; //immer mit Semmikolon abschließen <-- Initialisierung mit direktem Wert
                     // bzw.
         int age2;  // es gibt 2 Schreibweisen <-- Deklaration (Variable erzeugen "Datentyp" "Bezeichner")
-        age2 = 36; // "Zuweisung" (compiler Bezeichner wird mit dem Datentyp bekannt gemacht)
+        age2 = 5; // "Zuweisung" (compiler Bezeichner wird mit dem Datentyp bekannt gemacht)
+        //int age3 = 4;
+        //int birth_1 = 10.07.1987;
+        //int birth_2 = 16.01.2018;
+        //int birth_3 = 25.01.2019;
+        //int date = URLConnection //www.google.com/search?client=firefox-b-d&q=heutiges+datum
+        //String name = "David";
+        //String son1 = "Tim Milow";
+        //String son2 = "Theo Miles";
 
-        String name = David;
+        //System.out.println("Mein Name ist " + name + ", bin Papa von " + son1" und " + son2 + ".");
+        //System.out.println("Ich bin "+age". Mein Sohn "+ son1 + "ist "date - birth_2);
 
     }
 }
---
+++ src/einfache_rechnung.java
@@ -0,0 +1,9 @@
+public class einfache_rechnung {
+
+    public static void main(String[] args){
+
+        int number1 = 3 + 4;
+        System.out.println(number1);
+
+    }
+}
