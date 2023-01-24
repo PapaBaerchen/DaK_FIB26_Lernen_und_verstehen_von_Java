bd2f1defe659fd787e6a6f8330cd99cc070dfb26
dkoenig_FIB26 <dkoenig@computer-manufaktur.de>
2023-01-24 10:49:36 +0100

    alles neu

--- src/einfache_rechnung.java
+++ src/einfache_rechnung.java
@@ -3,7 +3,10 @@ public class einfache_rechnung {
     public static void main(String[] args){
 
         int number1 = 3 + 4;
-        System.out.println(number1);
+        int number2 = 5 + 7;
+        int result = number1+number2;
+
+        System.out.println(result);
 
     }
 }
