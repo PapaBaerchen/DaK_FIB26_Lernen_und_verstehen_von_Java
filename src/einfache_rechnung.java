2e701cd814d6ec83974f01a88cb8dc5d18afa1e5
dkoenig_FIB26 <dkoenig@computer-manufaktur.de>
2023-01-24 10:59:01 +0100

    alles neu

--- src/einfache_rechnung.java
+++ src/einfache_rechnung.java
@@ -6,7 +6,11 @@ public class einfache_rechnung {
         int number2 = 5 + 7;
         int result = number1+number2;
 
-        System.out.println(result);
+        System.out.println(result); // berechnung wird in result ausgeführt und ausgegeben
+        System.out.println(number1+number2); // berechnung findet in der Ausgabe statt
+        System.out.println(number1 + "" + number2); // gibt den ersten wert (number1) und danach den 2 wert (number2) an
+        //da /n fehlt sind die Zahlen direkt hintereinander ohne Lehrzeichen
+
 
     }
 }
