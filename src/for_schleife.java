5378b233d1de1edade787035ea3e30c1c48f33a1
dkoenig_FIB26 <dkoenig@computer-manufaktur.de>
2023-01-27 17:03:48 +0100

    alles neu

---
+++ src/for_schleife.java
@@ -0,0 +1,15 @@
+public class for_schleife {
+
+    static public void main(String[] args){
+        //findet anwendung, wenn man schon ganz genau weiß, wie oft die Schleife durchlaufen werden soll
+
+import.
+        for
+        (int i=0; //wert in der Variable wird gesetzt
+        i<10;
+        i++) {
+            System.out.println(i);
+        }
+    }
+
+}
