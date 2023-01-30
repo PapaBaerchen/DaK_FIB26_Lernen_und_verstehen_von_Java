bf24b4ec9bfa978decf86c590092aca6b08154a7
dkoenig_FIB26 <dkoenig@computer-manufaktur.de>
2023-01-30 14:27:55 +0100

    alles neu

--- src/for_schleife.java
+++ src/for_schleife.java
@@ -1,9 +1,10 @@
+import java.util.Scanner;
 public class for_schleife {
 
     static public void main(String[] args){
         //findet anwendung, wenn man schon ganz genau weiß, wie oft die Schleife durchlaufen werden soll
+    Scanner Tastatur = new Scanner(System.in);
 
-import.
         for
         (int i=0; //wert in der Variable wird gesetzt
         i<10;
