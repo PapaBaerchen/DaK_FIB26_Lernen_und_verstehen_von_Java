8ca910e9d374654ebb303eaad811d028303d9b17
dkoenig_FIB26 <dkoenig@computer-manufaktur.de>
2023-01-30 16:15:47 +0100

    alles neu

---
+++ src/verschachtelte_schleifen.java
@@ -0,0 +1,14 @@
+public class verschachtelte_schleifen {
+
+    static public void main(String [] args){
+
+        int test = 0;
+
+        for(int i=1; i< 4; i++){
+            for(int j= 1; j<4; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
