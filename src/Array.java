2909308834fd637f23cdb35f85de1126edd0d04c
dkoenig_FIB26 <dkoenig@computer-manufaktur.de>
2023-02-23 15:01:16 +0100

    alles neu

---
+++ src/Array.java
@@ -0,0 +1,19 @@
+public class Array {
+    public static void main(String[] args){
+
+        String[] names = new String [3]; // die Größe des Arrays wird immer bei der Initialisierung festgelegt ("[3]") begrenzt die Anzahl der verwendbaren Zeichen
+
+
+        names[0] = "David";
+        names[1] = "Frank";
+        names[2] = "Marcel";
+        /*
+        System.out.println(names[1]);
+        */
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+
+    }
+}
